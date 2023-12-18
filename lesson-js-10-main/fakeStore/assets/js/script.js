8bbea91d4c2277edf9930d90d1a6d791cd51aad6
GARAYEV-PG <rafkagarayev@gmail.com>
2023-12-18 21:31:09 +0400

    aa

--- lesson-js-10-main/fakeStore/assets/js/script.js
+++ lesson-js-10-main/fakeStore/assets/js/script.js
@@ -1,16 +1,17 @@
-const API_URL = 'https://fakestoreapi.com'
+const API_URL = "https://fakestoreapi.com";
 
-const productsContainer = document.querySelector('.products')
+const OpenModal = document.querySelector(".modal");
+const productsContainer = document.querySelector(".products");
 
 const fetchAllProducts = () => {
-    const res = fetch(`${API_URL}/products`)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(product => {
-                const productItem = document.createElement('div')
-                productItem.setAttribute('data-id', product.id)
-                productItem.classList = 'products-item'
-                productItem.innerHTML = `
+  const res = fetch(`${API_URL}/products`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((product) => {
+        const productItem = document.createElement("div");
+        productItem.setAttribute("data-id", product.id);
+        productItem.classList = "products-item";
+        productItem.innerHTML = `
                     <div class="products-item_img" data-id=${product.id}>
                         <img src=${product.image} alt=${product.title} data-id=${product.id}>
                     </div>
@@ -21,47 +22,46 @@ const fetchAllProducts = () => {
                         <span data-id=${product.id}>${product.category}</span>
                         <span data-id=${product.id}>${product.price} AZN</span>
                     </div>
-                `
-                productsContainer.appendChild(productItem)
+                `;
+        productsContainer.appendChild(productItem);
 
-                productItem.addEventListener('click', function(e) {
-                    console.log(e.target)
-                    const productId = e.target.getAttribute('data-id')
+        productItem.addEventListener("click", function (e) {
+          console.log(e.target);
+          const productId = e.target.getAttribute("data-id");
 
-
-                   
-fetch(`${API_URL}/products/${productId}`)
-                        .then((res) => res.json())
-                        .then((productData) => {
-                            const modalContent = document.createElement('div');
-                            modalContent.innerHTML = `
+          fetch(`${API_URL}/products/${productId}`)
+            .then((res) => res.json())
+            .then((productData) => {
+              const modalContent = document.createElement("div");
+              modalContent.innerHTML = `
                                 <h2>${productData.title}</h2>
                                 <img src="${productData.image}" alt="${productData.title}">
                                 <p>${productData.description}</p>
                                 <p>Price: ${productData.price} AZN</p>
                                 <button class="close-modal">Close</button>
                             `;
-            
-                            const modalOverlay = document.createElement('div');
-                            modalOverlay.classList.add('modal-overlay');
-                            modalOverlay.appendChild(modalContent);
-            
-                            
-                            OpenModal.appendChild(modalOverlay);
-            
-                            const closeModalButton = modalContent.querySelector('.close-modal');
-                            closeModalButton.addEventListener('click', function () {
-                                OpenModal.removeChild(modalOverlay);
-                            });
-                            console.log(productData);
-                        })
-                        .catch((error) => {
-                            console.error('Ürün getirme hatası:', error);
-                        });
-                        
-                })
-        })
-    })
-}
 
-fetchAllProducts()
+              const modalOverlay = document.createElement("div");
+              modalOverlay.classList.add("modal-overlay");
+              modalOverlay.appendChild(modalContent);
+
+              OpenModal.appendChild(modalOverlay);
+              OpenModal.style.display = "block";
+
+              const closeModalButton =
+                modalContent.querySelector(".close-modal");
+              closeModalButton.addEventListener("click", function () {
+                OpenModal.removeChild(modalOverlay);
+                OpenModal.style.display = "none";
+              });
+              console.log(productData);
+            })
+            .catch((error) => {
+              console.error("Ürün getirme hatası:", error);
+            });
+        });
+      });
+    });
+};
+
+fetchAllProducts();
